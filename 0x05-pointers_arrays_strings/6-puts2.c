04632258a2bcc4f1396176afb8a5520877ecf4f1
Judiiie <djseshoka@gmail.com>
2023-02-27 17:43:51 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,25 +1,25 @@
  #include "main.h"
-  2
-  3 /**
-  4  * puts2 - even index character of the str
-  5  * @str: pointer 
-  6  */
-  7
-  8 void puts2(char *str)
-  9 {
- 10         int l = 0, i = 0, j;
- 11         char *str_value = str;
- 12
- 13         while (*str_value != '\0')
- 14         {
- 15                 str_value++;
- 16                 l++;
- 17         }
- 18         i =  l - 1;
- 19         for (j = 0; j <= i; j++)
- 20         {
- 21                 if (j % 2 == 0)
- 22                         _putchar(str[j]);
- 23         }
- 24         _putchar('\n');
- 25 }
+
+/**
+ * puts2 - even index character of the str
+ * @str: pointer 
+ */
+
+void puts2(char *str)
+{
+	int l = 0, i = 0, j;
+        char *str_value = str;
+
+        while (*str_value != '\0')
+	{
+		str_value++;
+		l++;
+	}
+	i =  l - 1;
+	for (j = 0; j <= i; j++)
+	{
+		if (j % 2 == 0)
+			_putchar(str[j]);
+	}
+	_putchar('\n');
+}
