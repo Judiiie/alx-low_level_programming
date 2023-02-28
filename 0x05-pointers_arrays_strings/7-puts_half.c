6a0b389ff29130d69ac2d0bebf1642409e08dc69
Judiiie <djseshoka@gmail.com>
2023-02-27 18:48:03 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,10 +11,10 @@ void puts_half(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 		ln = ln + 1;
-       	n = ln / 2;
+	n = ln / 2;
 	if ((ln % 2) == 1)
 		n = ((ln + 1) / 2);
 	for (i = n; str[i] != '\0'; i++)
 		_putchar(str[i]);
-       	_putchar('\n');
+	_putchar('\n');
 }
