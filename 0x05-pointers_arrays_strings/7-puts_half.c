2c1a9b79993e3419e9a3f814f0df7ea8aeb6f41a
Judiiie <djseshoka@gmail.com>
2023-02-27 18:35:15 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  puts_half - get the string half characters
+ *  @str: pointer
+ */
+
+  void puts_half(char *str)
+{
+	int ln = 0, i, n;
+
+	for (i = 0; str[i] != '\0'; i++)
+		ln = ln + 1;
+       	n = ln / 2;
+	if ((ln % 2) == 1)
+		n = ((ln + 1) / 2);
+	for (i = n; str[i] != '\0'; i++)
+		_putchar(str[i]);
+       	_putchar('\n');
+}
