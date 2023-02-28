4225d2f59c627bc7cdaeb4caf1ec641fee35563c
Judiiie <djseshoka@gmail.com>
2023-02-27 19:05:35 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,16 +7,16 @@
 
 void rev_string(char *s)
 {
-        int i, counter = 0;
-        char rev_string = s[0];
-
-        while (s[counter] != '\0')
+	int i, counter = 0;
+	char rev_string = s[0];
+       
+	while (s[counter] != '\0')
 		counter++;
 	
 	for (i = 0; i < counter; i++)
         {
 		counter--;
-		rev_string = s[counter];
+		rev_string = s[i];
 		s[counter] = rev_string;
         }
 }
