567cda9a8c6b9eaaa1e0ad356951033a65aee109
Judiiie <djseshoka@gmail.com>
2023-02-28 06:13:33 -0800

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Function start the program execution
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int s;
+	char c;
+
+	srand(time(0));
+	while (s <= 2645)
+	{
+		c = rand() %128;
+		s += c;
+		putchar(c);
+	}
+	putchar(2772 - s);
+
+	return (0)
+}
