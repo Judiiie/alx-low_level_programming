9ed648cc9fd1cf4aeca73829350e98fdf2ce2117
Judiiie <djseshoka@gmail.com>
2023-03-04 05:21:04 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,46 @@
 #include "main.h"
 
 /**
- * rot13 - encoding a string using rot13
- * @s: string to be encoded
- * Return: the encoded string
+ * rot13 - Encodes a string using rot13.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
  */
-
-char *rot13(char *s)
+char *rot13(char *str)
 {
-	int i; ln;
+	int indx1 = 0, indx2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
 
-	ln = strlen(s);
-
-	for (i = 0; i < ln; i++)
+	while (str[indx1])
 	{
-		if ((*(s + i) >= 'a' && *(s + i) < 'n') || 
-				(*(s + i) >= 'A' && *(s + i) < 'N'))
-					*(s + i) += 13;
-		else ((*(s + i) >= 'm' && *(s + i) < 'z') || 
-			(*(s + i) >= 'M' && *(s + i) < 'Z'))
-				*(s + i) += -13;
+		for (indx2 = 0; indx2 < 52; indx2++)
+		{
+			if (str[indx1] == alphabet[indx2])
+			{
+				str[indx1] = rot13key[indx2];
+				break;
+			}
+		}
+
+		indx1++;
+
 	}
-	return (s);
+	return (str);
 }
