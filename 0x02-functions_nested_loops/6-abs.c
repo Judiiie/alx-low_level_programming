657b2d3da06e3edee542aaac868cf9c05be99a0d
Judiiie <djseshoka@gmail.com>
2023-03-04 12:00:50 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_abs - Function calculates the value of an int
+*@n: The function takes a single argument int n
+*Return: The generated absolute value
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	return (-n);
+}
+
