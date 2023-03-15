507ab72dc75e53098ab4cc147a8746d923aa08d5
Judiiie <djseshoka@gmail.com>
2023-03-15 15:26:26 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
  * main - Program that takes in all integer arguments and returns the sum
@@ -39,4 +41,3 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
-
