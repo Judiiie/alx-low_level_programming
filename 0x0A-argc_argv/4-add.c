d6da56d16104a5741ddf72333cd610e263328c92
Judiiie <djseshoka@gmail.com>
2023-03-15 15:18:15 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int numeric(char *s);
-
 /**
- * main - A program that adds positive numbers
- * @argc: argument commands counts
- * @argv: argument commands values
- * Return: 0
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
  */
 
 int main(int argc, char *argv[])
 {
-	int sum = 0, a;
+	int i, j, length, sum;
+	char *ptr;
 
-	for (a = 1; a < argc; a++)
+	if (argc < 2)
+	printf("0\n");
+	else
 	{
-		if (numeric(argv[a]) == 0)
+		sum = 0;
+		for (i = 1; i < argc; i++)
 		{
-			puts("Error");
-			return (1);
-		}
-		sum += atoi(argv[a]);
-	}
-	printf("%d\n", sum);
+			ptr = argv[i];
+			length = strlen(ptr);
 
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
+		}
+
+		printf("%d\n", sum);
+	}
 	return (0);
 }
+
